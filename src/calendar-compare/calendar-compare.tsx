c9823ad5f003dfc08f7b35d93aa54c16e4c3be6f
Tim Chinenov <timchinenov@Tims-Laptop.fios-router.home>
2023-01-09 16:02:22 -0500

    Added hours to calendar

--- src/calendar-compare/calendar-compare.tsx
+++ src/calendar-compare/calendar-compare.tsx
@@ -1,3 +1,4 @@
+import { DateInput } from "date-picker-svelte";
 import moment, { MomentZone } from "moment-timezone";
 import { useState } from "react";
 import Calendar from "../calendar/calendar";
@@ -9,6 +10,10 @@ export function CalendarCompare() {
     const [firstTimezone, setFirstTimezone] = useState<MomentZone | null>();
     const [secondTimezone, setSecondTimezone] = useState<MomentZone | null>();
 
+    const [meetingLength, setMeetingLength] = useState(0);
+
+    let date = new Date();
+
     const timezones: string[] = moment.tz.names();
     const timezonesSelection: any[] = []
     timezones.forEach((timezone) => {
@@ -19,18 +24,37 @@ export function CalendarCompare() {
 
     return(
         <div className="grid grid-cols-2 place-items-center">
-            <div className="grid-rows-3">
-                <p>{ firstLocation }</p>
-                <p>{ firstTimezone?.offsets[0]}</p>
-                <select
-                    className="select w-full max-w-xs"
-                    onChange={(event) => {
-                        setFirstLocation(event.target.value)
-                        setFirstTimezone(moment.tz.zone(event.target.value))
-                    }}>
-                    <option disabled selected>Select location</option>
-                    { timezonesSelection }
-                </select>
+            <div className="grid-rows-4">
+                <div>
+                    <p>{ firstLocation }</p>
+                    <p>{ firstTimezone?.offsets[0]}</p>
+                </div>
+                
+                <div>
+                    <select
+                        className="select w-full max-w-xs"
+                        onChange={(event) => {
+                            setFirstLocation(event.target.value)
+                            setFirstTimezone(moment.tz.zone(event.target.value))
+                        }}>
+                        <option disabled selected>Select location</option>
+                        { timezonesSelection }
+                    </select>
+                </div>
+
+                <div>
+                    <input 
+                        type="date"
+                        className="input input-bordered w-full max-w-xs"/>
+                </div>
+                
+                <div>
+                    <input
+                        type="number"
+                        placeholder="Meeting Length"
+                        onBlur={(event) => setMeetingLength(parseInt(event.target.value))} 
+                        className="input input-bordered w-full max-w-xs" />
+                </div>
             </div>
             
             <div className="grid-rows-3">
@@ -49,11 +73,11 @@ export function CalendarCompare() {
             
 
             <div className="w-3/4">
-                <Calendar />
+                <Calendar meetingLength={meetingLength} offset={0} date={date}/>
             </div>
 
             <div className="w-3/4">
-                <Calendar />
+                <Calendar meetingLength={meetingLength} offset={0} date={date}/>
             </div>
         </div>
     )
--- src/calendar/calendar.tsx
+++ src/calendar/calendar.tsx
@@ -1,81 +1,106 @@
 import { useState } from "react";
 
-export default function Calendar() {
+export default function Calendar(
+    {
+        meetingLength = 0,
+        offset = 0,
+        date,
+    } : { meetingLength: number, offset: number, date: Date}) {
 
     const [boxStart, setBoxStart] = useState(0);
-    const [length, setLength] = useState(0);
 
     let hourSections: any[] = []
     
     for(var count = 0; count < 25; count++) {
         const currentCount = count * 10
         hourSections.push(
-        <div className="
-            w-full
-            h-24
-            border-t-[1px] border-t-slate-50 border-b-slate-50">
-                <div
-                    className="relative h-6 border-t-[1px] border-t-slate-500 border-b-slate-500 hover:border-t-red-300 border-t-[2px]"
-                    onClick={() => {
-                        setBoxStart(currentCount + 2)
-                        setLength(2);
-                    }}>
-                        { (boxStart && boxStart == currentCount + 2) &&
-                            <div
-                                className={`w-3/4 border-red-500 bg-cyan-500 absolute z-10 rounded mx-6`}
-                                style={{height: `${24*length}px`}}>
-                            </div>
-                        }
+            <div className="grid grid-cols-6">
+                <div className="col-span-1 border-t-[1px] border-t-slate-50 border-b-slate-50">
+                    { incrementToTime(count, 1) }
                 </div>
-                <div
-                    className="relative h-6 border-t-[1px] border-t-slate-500 border-b-slate-500 hover:border-t-red-300"
-                    onClick={() => {
-                        setBoxStart(currentCount + 4)
-                        setLength(2);
-                    }}>
-                        { (boxStart && boxStart == currentCount + 4) &&
-                            <div 
-                                className={`w-3/4 border-red-500 bg-cyan-500 absolute z-10 rounded mx-6`}
-                                style={{height: `${24*length}px`}}>
-                            </div>
-                        }
+                <div className="
+                    col-span-5
+                    w-full
+                    h-24
+                    border-t-[1px] border-t-slate-50 border-b-slate-50">
+                    <div
+                        className="relative h-6 border-t-[1px] border-t-slate-500 border-b-slate-500 hover:border-t-red-300 border-t-[2px]"
+                        onClick={() => {
+                            setBoxStart(currentCount + 2)
+                        }}>
+                            { (boxStart && boxStart == currentCount + 2) &&
+                                <div
+                                    className={`w-3/4 border-red-500 bg-cyan-500 absolute z-10 rounded mx-6`}
+                                    style={{height: `${24 * meetingLength}px`}}>
+                                </div>
+                            }
+                    </div>
+                    <div
+                        className="relative h-6 border-t-[1px] border-t-slate-500 border-b-slate-500 hover:border-t-red-300"
+                        onClick={() => {
+                            setBoxStart(currentCount + 4)
+                        }}>
+                            { (boxStart && boxStart == currentCount + 4) &&
+                                <div 
+                                    className={`w-3/4 border-red-500 bg-cyan-500 absolute z-10 rounded mx-6`}
+                                    style={{height: `${24 * meetingLength}px`}}>
+                                </div>
+                            }
+                    </div>
+                    <div
+                        className="relative h-6 border-t-[1px] border-t-slate-500 border-b-slate-500 hover:border-t-red-300"
+                        onClick={() => {
+                            setBoxStart(currentCount + 6)
+                        }}>
+                            { (boxStart && boxStart == currentCount + 6) &&
+                                <div
+                                    className={`w-3/4 border-red-500 bg-cyan-500 absolute z-10 rounded mx-6`}
+                                    style={{height: `${24 * meetingLength}px`}}>
+                                </div>
+                            }
+                    </div>
+                    <div
+                        className="relative h-6 border-t-[1px] border-t-slate-500 border-b-slate-500 hover:border-t-red-300"
+                        onClick={() => {
+                            setBoxStart(currentCount + 8)
+                        }}>
+                            { (boxStart && boxStart == currentCount + 8) &&
+                                <div
+                                    className={`w-3/4 border-red-500 bg-cyan-500 absolute z-10 rounded mx-6`}
+                                    style={{height: `${24 * meetingLength}px`}}>
+                                </div>
+                            }
+                    </div>
                 </div>
-                <div
-                    className="relative h-6 border-t-[1px] border-t-slate-500 border-b-slate-500 hover:border-t-red-300"
-                    onClick={() => {
-                        setBoxStart(currentCount + 6)
-                        setLength(2);
-                    }}>
-                        { (boxStart && boxStart == currentCount + 6) &&
-                            <div
-                                className={`w-3/4 border-red-500 bg-cyan-500 absolute z-10 rounded mx-6`}
-                                style={{height: `${24*length}px`}}>
-                            </div>
-                        }
-                </div>
-                <div
-                    className="relative h-6 border-t-[1px] border-t-slate-500 border-b-slate-500 hover:border-t-red-300"
-                    onClick={() => {
-                        setBoxStart(currentCount + 8)
-                        setLength(2);
-                    }}>
-                        { (boxStart && boxStart == currentCount + 8) &&
-                            <div
-                                className={`w-3/4 border-red-500 bg-cyan-500 absolute z-10 rounded mx-6`}
-                                style={{height: `${24*length}px`}}>
-                            </div>
-                        }
-                </div>
-        </div>)
+            </div>
+        )
     }
 
     return (
         <div className="h-full w-full">
             <h2>time selected: { boxStart }</h2>
-            <h2>length: { length }</h2>
+            <h2>length: { meetingLength }</h2>
             <div className="h-full w-full border-b-[1px]">
                 { hourSections }
             </div>
         </div>
     )
+}
+
+function incrementToTime(count: number, offset: number): string {
+    let isAfternoon = false
+    let time = "12:00"
+
+    if (count > 11) {
+        count = count - 12
+        isAfternoon = true
+    }
+
+    if (count != 0) {
+        time = `${count}:00`
+    }
+
+    time += isAfternoon ? " PM" : " AM"
+
+    return time
 }
