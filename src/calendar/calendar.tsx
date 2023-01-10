22fc1dc1f1ca25fd9cf3516ae877d4940b4ae8ac
Tim Chinenov <timchinenov@Tims-Laptop.fios-router.home>
2023-01-10 02:24:37 -0500

    adding space

--- src/calendar/calendar.tsx
+++ src/calendar/calendar.tsx
@@ -97,7 +97,7 @@ export default function Calendar({
     }
 
     return (
-        <div className="h-full w-full">
+        <div className="h-full w-full ">
             <div className="h-full w-full border-b-[1px]">
                 { hourSections }
             </div>
